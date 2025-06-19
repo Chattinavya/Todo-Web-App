f298f700de02eec8bee82921e8d52f0cfad23996
chattinavya <chattinavya@gmail.com>
2025-06-19 10:27:13 +0530

    your message

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Todo-Web-App/', // 👈 Important: Use repo name with slashes
   plugins: [react()],
-})
+});
